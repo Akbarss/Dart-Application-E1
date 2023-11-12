ab60018050070a7b38fdd561f6b3114cf52b778d
Akbarss <kbrgulyamov@gmail.com>
2023-11-12 17:32:03 +0500

    feat: type add

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,5 +1,17 @@
-import 'package:dart_application/dart_application.dart' as dart_application;
+var num = 0;
+var word = 'Hello';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application.calculate()}!');
+void main() {
+  double point = 4.5;
+  point++;
+
+  // ignore: unused_local_variable
+  String some;
+  some = 'Hello Dart';
+
+  // ignore: unused_local_variable
+  bool isDart;
+  isDart = true;
+
+  print(point);
 }
